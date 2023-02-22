43e8f07ecc43b76da1d6aae7ac51215a9ba40cc3
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-02-22 15:41:23 -0300

    Add files via upload

---
+++ ExerciciosEstruturaCondicional/Exercicio1/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio2 {
+    class Program {
+        static void Main(string[] args) {
+
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0) {
+                Console.WriteLine("PAR");
+            }
+            else {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
---
+++ ExerciciosEstruturaCondicional/Exercicio1/Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio3 {
+    class Program {
+        static void Main(string[] args) {
+
+            int A, B;
+
+            string[] valores = Console.ReadLine().Split();
+            A = int.Parse(valores[0]);
+            B = int.Parse(valores[1]);
+
+            if (A % B == 0 || B % A == 0) {
+                Console.WriteLine("São Multiplos");
+            }
+            else {
+                Console.WriteLine("Não são Multiplos");
+            }
+        }
+    }
+}
