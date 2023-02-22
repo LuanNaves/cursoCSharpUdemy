7c68854aca55a8fc510111f58938b6410a30a0fb
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-02-22 15:43:58 -0300

    Add files via upload

---
+++ ExerciciosEstruturaCondicional/Exercicio1/Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio5 {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] valores = Console.ReadLine().Split();
+
+            int item = int.Parse(valores[0]);
+            int quant = int.Parse(valores[1]);
+            double preço, total;
+
+            if (item == 1) {
+                preço = 4.00;
+            }
+            else if (item == 2) {
+                preço = 4.50;
+            }
+            else if (item == 3) {
+                preço = 5.00;
+            }
+            else if (item == 4) {
+                preço = 2.00;
+            }
+            else {
+                preço = 1.50;
+            }
+
+            total = preço * quant;
+            Console.WriteLine($"Total: R$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
