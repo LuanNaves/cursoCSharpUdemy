eaf1524c3e35f68af71a0c66057e90910a6dccf3
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-03-03 15:37:22 -0300

    Add files via upload

---
+++ ExerciciosEstruturaFor/Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio3 {
+    class Program {
+        static void Main(string[] args) {
+
+            int casos = int.Parse(Console.ReadLine());
+            double valor1, valor2, valor3, media;
+
+            for (int i = 1; i <= casos; i++) {
+
+                string[] valores = Console.ReadLine().Split();
+                valor1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+                valor2 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+                valor3 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+                media = (valor1 * 2 + valor2 * 3 + valor3 * 5) / 10;
+
+                Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
---
+++ ExerciciosEstruturaFor/Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio4 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+            int numerador, denominador;
+
+            for (int i = 1; i <= n; i++) {
+
+                string[] valores = Console.ReadLine().Split();
+                numerador = int.Parse(valores[0]);
+                denominador = int.Parse(valores[1]);
+
+                if (denominador == 0) {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else {
+                    double divisao = (double)numerador / denominador;
+                    Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
