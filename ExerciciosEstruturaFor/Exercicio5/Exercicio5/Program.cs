be4c975862895132ff15bd6307d4e7f95948767b
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-03-03 15:38:14 -0300

    Add files via upload

---
+++ ExerciciosEstruturaFor/Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio5 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
+            int fatorial = 1;
+
+            for (int i = 1; i <= n; i++) {
+                fatorial = fatorial * i;
+            }
+
+            Console.WriteLine(fatorial);
+        }
+    }
+}
