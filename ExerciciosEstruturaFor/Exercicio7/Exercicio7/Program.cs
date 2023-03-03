8140e5d62b7d9094c10cb102ea0877c9d717187b
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-03-03 15:40:43 -0300

    Add files via upload

---
+++ ExerciciosEstruturaFor/Exercicio7/Exercicio7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio7 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+            int quad, cubo;
+
+            for (int i = 1; i <= n; i++) {
+                quad = (int)Math.Pow(i, 2);
+                cubo = (int)Math.Pow(i, 3);
+
+                Console.WriteLine($"{i} {quad} {cubo}");
+            }
+        }
+    }
+}
