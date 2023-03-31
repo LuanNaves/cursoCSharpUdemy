540397c0329e21b911fefe15bd3ffb0021214350
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-03-31 13:42:22 -0300

    Add files via upload

---
+++ ExercicioVetores/Exercicio1/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Exercicio1 {
+    internal class Student {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public Student(string name, string email, int room) { 
+            Name = name;
+            Email = email;
+            Room = room;
+        }
+
+        public override string ToString() {
+            return Room + ": " 
+                + Name + ", " 
+                + Email;
+        }
+    }
+}
