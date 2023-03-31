93293c0dbedbdd40e83c3cacf16d5ef051e59622
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-03-31 13:32:12 -0300

    Add files via upload

---
+++ ExerciciosMembrosEstáticos/Exercicio1/Exercicio1/ConversorDeMoeda.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercicio1 {
+    internal class ConversorDeMoeda {
+
+        public static double Cotação, Valor;
+        public static double ValorConvertido() {
+            return (Valor * 0.06 + Valor) * Cotação;
+        }
+    }
+}
