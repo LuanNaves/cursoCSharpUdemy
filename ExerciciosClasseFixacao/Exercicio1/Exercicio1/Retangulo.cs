76716a95661f002598523b72fe282faeef4dcd1e
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-03-31 13:35:52 -0300

    Add files via upload

---
+++ ExerciciosClasseFixacao/Exercicio1/Exercicio1/Retangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1 {
+    internal class Retangulo {
+        public double Largura, Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() {
+            return Largura * 2 + Altura * 2;
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+    }
+}
---
+++ ExerciciosClasseFixacao/Exercicio2/Exercicio2/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio2 {
+    internal class Funcionario {
+        public string Nome;
+        public double SalarioBruto, Imposto;
+
+        public double SalarioLiquido() {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            SalarioBruto += porcentagem / 100 * SalarioBruto;
+        }
+
+        public override string ToString() {
+            return Nome + ", R$" + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
