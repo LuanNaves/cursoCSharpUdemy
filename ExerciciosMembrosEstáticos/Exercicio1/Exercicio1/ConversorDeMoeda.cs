fa12edcc0817e10ad75e0b6d3f8b9f9ef6f9a47e
Luan Felipe <106930442+LuanNaves@users.noreply.github.com>
2023-03-31 13:40:22 -0300

    Delete ExerciciosMembrosEstáticos directory

--- ExerciciosMembrosEstáticos/Exercicio1/Exercicio1/ConversorDeMoeda.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Exercicio1 {
-    internal class ConversorDeMoeda {
-
-        public static double Cotação, Valor;
-        public static double ValorConvertido() {
-            return (Valor * 0.06 + Valor) * Cotação;
-        }
-    }
-}
